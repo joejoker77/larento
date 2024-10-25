36706d789c9b49608ffa5e867c74e858c9f0c9e6
Yanovskiy Vitaliy <joejoker77@gmail.com>
2024-10-25 13:52:50 +0300

    add yandex metrika

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -12,6 +12,23 @@
     <meta name="yandex-verification" content="d3f4ab31cc7e1109" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"/>
     @vite('resources/scss/app.scss')
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(98728990, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true
+        });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/98728990" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
 </head>
 <body>
 <div class="container p-0 site-wrapper @if(Route::current()->getName() === 'home') main @endif">
