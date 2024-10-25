97f28d4f30682bda9a429e0952cbf200339b3271
Yanovskiy Vitaliy <joejoker77@gmail.com>
2024-10-25 13:46:15 +0300

    add yandex webmaster

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -9,6 +9,7 @@
     {!! Meta::toHtml() !!}
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
+    <meta name="yandex-verification" content="d3f4ab31cc7e1109" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"/>
     @vite('resources/scss/app.scss')
 </head>
