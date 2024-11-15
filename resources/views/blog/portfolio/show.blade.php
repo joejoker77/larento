67e9429b0ac84c61c2e75f416053a4d5ad043f1a
Yanovskiy Vitaliy <joejoker77@gmail.com>
2024-11-15 11:48:57 +0300

    fix portfolio page two

--- resources/views/blog/portfolio/show.blade.php
+++ resources/views/blog/portfolio/show.blade.php
@@ -10,41 +10,43 @@
                 </aside>
             </div>
             <div class="col-lg-9">
-                <main-gallery data-sizeReplace="large" class="col-md-7">
-                    <div class="swiper full-swiper">
-                        <div class="swiper-wrapper">
-                            @php /** @var App\Entities\Shop\Photo $photo */ @endphp
-                            @foreach($post->photos as $photo)
-                                <div class="swiper-slide full">
-                                    <img src="{{ $photo->getPhoto('large') }}" alt="{{ $photo->alt_tag }} @if($photo->alt_tag) размер большой @endif">
-                                </div>
-                            @endforeach
-                        </div>
-                        @if($post->photos->count() > 1)
-                            <div class="swiper-button swiper-button-next"></div>
-                            <div class="swiper-button swiper-button-prev"></div>
-                        @endif
-                        <div class="gallery-controls">
-                            <div class="full-screen-button">
-                                <span class="open material-symbols-outlined" data-bs-placement="left" data-bs-custom-class="custom-tooltip" data-bs-toggle="tooltip" data-bs-title="Полноэкранный режим">fullscreen</span>
-                                <span class="close material-symbols-outlined d-none" data-bs-placement="left" data-bs-custom-class="custom-tooltip" data-bs-toggle="tooltip" data-bs-title="Выйти из полноэкранного режима">close_fullscreen</span>
-                            </div>
-                        </div>
-                    </div>
-                    @if($post->photos->count() > 1)
-                        <div class="swiper thumbs-swiper">
+                <div class="d-flex flex-nowrap flex-column flex-md-row justify-content-between pb-2 mb-3 pb-lg-5 mb-lg-5 border-bottom portfolio_item">
+                    <main-gallery data-sizeReplace="large" class="col-md-7">
+                        <div class="swiper full-swiper">
                             <div class="swiper-wrapper">
+                                @php /** @var App\Entities\Shop\Photo $photo */ @endphp
                                 @foreach($post->photos as $photo)
-                                    <div class="swiper-slide thumb">
-                                        <img src="{{ $photo->getPhoto('small') }}" alt="{{ $photo->alt_tag }} @if($photo->alt_tag) размер маленький @endif">
+                                    <div class="swiper-slide full">
+                                        <img src="{{ $photo->getPhoto('large') }}" alt="{{ $photo->alt_tag }} @if($photo->alt_tag) размер большой @endif">
                                     </div>
                                 @endforeach
                             </div>
+                            @if($post->photos->count() > 1)
+                                <div class="swiper-button swiper-button-next"></div>
+                                <div class="swiper-button swiper-button-prev"></div>
+                            @endif
+                            <div class="gallery-controls">
+                                <div class="full-screen-button">
+                                    <span class="open material-symbols-outlined" data-bs-placement="left" data-bs-custom-class="custom-tooltip" data-bs-toggle="tooltip" data-bs-title="Полноэкранный режим">fullscreen</span>
+                                    <span class="close material-symbols-outlined d-none" data-bs-placement="left" data-bs-custom-class="custom-tooltip" data-bs-toggle="tooltip" data-bs-title="Выйти из полноэкранного режима">close_fullscreen</span>
+                                </div>
+                            </div>
                         </div>
-                    @endif
-                </main-gallery>
-                <div class="col-lg-5 d-flex flex-column description">
-                    {!! $post->content !!}
+                        @if($post->photos->count() > 1)
+                            <div class="swiper thumbs-swiper">
+                                <div class="swiper-wrapper">
+                                    @foreach($post->photos as $photo)
+                                        <div class="swiper-slide thumb">
+                                            <img src="{{ $photo->getPhoto('small') }}" alt="{{ $photo->alt_tag }} @if($photo->alt_tag) размер маленький @endif">
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                        @endif
+                    </main-gallery>
+                    <div class="col-lg-5 d-flex flex-column description">
+                        {!! $post->content !!}
+                    </div>
                 </div>
             </div>
         </div>
